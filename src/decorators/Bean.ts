98ccc5cb2f357c3d41b400262bd693544bacd63a
Artem Kornev <artem.kornev@y-tree.com>
2020-08-18 19:41:43 +0300

    Added types for Bean

--- src/decorators/Bean.ts
+++ src/decorators/Bean.ts
@@ -1,5 +1,27 @@
-export const Bean = <T extends Function>(
+type TBeanScope = 'prototype' | 'singleton';
+type TBeanProps = {
+    scope?: TBeanScope;
+    name?: string;
+}
+
+export function Bean<T>(
     target: any,
     propertyKey: string | symbol,
     descriptor: TypedPropertyDescriptor<T>,
-): void => {};
+): void;
+
+export function Bean<T>(
+    beanProps: TBeanProps,
+): typeof Bean;
+
+export function Bean<T>(...args: any[]) {
+    if (args.length === 3) {
+        return;
+    }
+
+    return function (
+        target: any,
+        propertyKey: string | symbol,
+        descriptor: TypedPropertyDescriptor<T>,
+    ): void {}
+}
