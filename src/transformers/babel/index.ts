ed73004e9c863707fa9f08ab23ac3b4bae480dd0
Artem Kornev <artemkakornev1@gmail.com>
2021-02-05 00:12:21 +0200

    transformers moved to another directory

--- src/transformers/babel/index.ts
+++ src/transformers/babel/index.ts
@@ -1,3 +1,53 @@
-import babelTransformer from '../../external/transformers/babel';
+import ts from 'typescript';
+import { IDiConfig, initDiConfig } from '../../external/config';
+import { runCompile } from '../../internal/runCompile';
+import { logLogo } from '../../external/transformers/logLogo';
+import { getTransformerFactory } from '../../external/transformers/getTransformerFactory';
+import { libraryName } from '../../constants/libraryName';
+import { ProgramRepository } from '../../internal/program/ProgramRepository';
 
-export default babelTransformer;
+logLogo();
+
+export default function(api: any, options?: IDiConfig) {
+    initDiConfig(options);
+    runCompile();
+    const transformerFactory = getTransformerFactory();
+    const printer = ts.createPrinter();
+
+    return {
+        visitor: {
+            Program(path: any, meta: any) {
+                const imports: any[] = path.node.body.filter((it: any) => it.type === 'ImportDeclaration');
+                const hasLibraryImport = imports.some(it => {
+                    const moduleSpecifier = it?.source?.value;
+                    if (!moduleSpecifier) {
+                        return false;
+                    }
+                    return moduleSpecifier === libraryName;
+                });
+
+                if (!hasLibraryImport) {
+                    return;
+                }
+
+                const fileText = meta.file.code;
+                const filePath = meta.filename;
+                const tsSourceFile = ts.createSourceFile(
+                    filePath,
+                    fileText,
+                    ProgramRepository.program.getCompilerOptions().target ?? ts.ScriptTarget.ES2015,
+                    true,
+                );
+                const result = ts.transform<ts.SourceFile>(
+                    tsSourceFile,
+                    [transformerFactory],
+                );
+                const resultText = printer.printFile(result.transformed[0]);
+
+                path.node.body = api.parse(resultText, {
+                    plugins: meta.file.opts.plugins
+                }).program.body;
+            }
+        }
+    };
+}
--- src/transformers/typescript/index.ts
+++ src/transformers/typescript/index.ts
@@ -1,3 +1,14 @@
-import transformer from '../../external/transformers/typescript';
+import ts from 'typescript';
+import { IDiConfig, initDiConfig } from '../../external/config';
+import { runCompile } from '../../internal/runCompile';
+import { getTransformerFactory } from '../../external/transformers/getTransformerFactory';
+import { logLogo } from '../../external/transformers/logLogo';
 
-export default transformer;
+logLogo();
+
+export default (program: ts.Program, config?: IDiConfig): ts.TransformerFactory<ts.SourceFile> => {
+    initDiConfig(config);
+    runCompile();
+
+    return getTransformerFactory();
+};
