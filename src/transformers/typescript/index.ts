1a2800488128445f3171398e46b3658bedfe95a5
lannav <evheniy.sytnik@y-tree.com>
2021-04-07 12:19:07 +0300

    moved logo print to the initDiConfig

--- src/transformers/typescript/index.ts
+++ src/transformers/typescript/index.ts
@@ -2,15 +2,9 @@ import ts from 'typescript';
 import { IDiConfig, initDiConfig } from '../../external/config';
 import { runCompile } from '../../core/runCompile';
 import { getTransformerFactory } from '../../core/transformers/getTransformerFactory';
-import { logLogo } from '../../core/transformers/logLogo';
 
 export default (program: ts.Program, config?: IDiConfig): ts.TransformerFactory<ts.SourceFile> => {
     initDiConfig(config);
-
-    if (!config?.disableLogoPrint) {
-        logLogo();
-    }
-
     runCompile();
 
     return getTransformerFactory();
