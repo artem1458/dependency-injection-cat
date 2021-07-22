7bfbf88a5de4045162b8209fd3d7ad6c9c73c4e3
Artem Kornev <artemkakornev1@gmail.com>
2021-07-23 02:14:31 +0300

    fix context name

--- tests/test/test.test.ts
+++ tests/test/test.test.ts
@@ -4,7 +4,7 @@ import { IBeans } from '../src/config/Context.di';
 describe('test', () => {
     it('container', () => {
         console.log(container.initContext<IBeans>({
-            name: 'Context'
+            name: 'MainContext'
         }));
     });
 });
