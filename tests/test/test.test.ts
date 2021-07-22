3162bf76501861ab5990057e4d26702d8d0db5e3
Artem Kornev <artemkakornev1@gmail.com>
2021-07-23 02:25:52 +0300

    fix import

--- tests/test/test.test.ts
+++ tests/test/test.test.ts
@@ -1,5 +1,5 @@
 import { container } from 'dependency-injection-cat';
-import { IBeans } from '../src/config/Context.di';
+import { IBeans } from '../src/config/IBeans';
 
 describe('test', () => {
     it('container', () => {
