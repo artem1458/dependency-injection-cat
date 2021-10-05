5ef456a5808d4da5ce80cb972f40c91e37c5e17e
Artem Kornev <artemkakornev1@gmail.com>
2021-10-05 10:51:25 +0300

    upgrade_type_qualifier

--- debug/TestContext.di.ts
+++ debug/TestContext.di.ts
@@ -8,4 +8,11 @@ export interface XZ<T> {
 
 class TestContext extends CatContext<ITestContext> {
     @Bean test: string & XZ<string & number> = [] as any;
+
+    @Bean
+    xz(
+        d: string & XZ<string & number>,
+    ): void {
+        d;
+    }
 }
--- src/core/utils/ExtendedSet.ts
+++ src/core/utils/ExtendedSet.ts
@@ -2,4 +2,8 @@ export class ExtendedSet<T = unknown> extends Set<T> {
     empty(): boolean {
         return this.size === 0;
     }
+
+    list(): T[] {
+        return Array.from(this);
+    }
 }
