97e676aeaebfb43bea1063f17f2364982eb2b837
Artem Kornev <artemkakornev1@gmail.com>
2022-07-09 18:13:16 +0300

    feature/intellij_plugin_preparations

--- src/service/index.ts
+++ src/service/index.ts
@@ -2,9 +2,11 @@ import { DICatService } from './DICatService';
 import { FileSystemHandler } from './handlers/FileSystemHandler';
 import { ProcessFilesHandler } from './handlers/ProcessFilesHandler';
 
-const service = new DICatService(
-    new FileSystemHandler(),
-    new ProcessFilesHandler(),
-);
+(async () => {
+    const service = new DICatService(
+        new FileSystemHandler(),
+        new ProcessFilesHandler(),
+    );
 
-service.run();
+    await service.run();
+})();
